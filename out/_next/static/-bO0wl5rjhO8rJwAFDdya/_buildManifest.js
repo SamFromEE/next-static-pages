d79f3edcf8a1bee9a590c43bac1255240c320a41
Sam Plews <the@codeite.com>
2024-08-01 17:17:51 +0100

    Add ouput for GitHub pages

---
+++ out/_next/static/-bO0wl5rjhO8rJwAFDdya/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-d283e3893a9f83aa.js"],"/_error":["static/chunks/pages/_error-0bb032ccac409a35.js"],"/users/[user]":["static/chunks/pages/users/[user]-19c5e824b1113778.js"],sortedPages:["/","/_app","/_error","/users/[user]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
